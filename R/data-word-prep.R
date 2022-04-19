2f9d3e0aeb2b826a65e3899e2c1360c1aae3e11f
Camila Achuri <camilaachury.4@gmail.com>
2022-04-19 19:45:54 -0300

    separate dic to remove words in text and add options in function data_word_prep

--- R/data-word-prep.R
+++ R/data-word-prep.R
@@ -3,24 +3,27 @@
 #' @param data A data frame with geografical info.
 #' @param ftype A string value with type of data to be plotted
 #' @export
-data_word_prep <- function(data, ftype = NULL) {
+data_word_prep <- function(data, ftype = NULL, lang = "es", stopwords = FALSE) {
 
   if (any(class(data) %in% c("data.frame", "fringe", "tbl_df","tbl"))) {
    if (ftype == "Cat") {
    data <- paste0(data[[1]], sep = " ", collapse = " ")
    }
   }
- stopwords <- readr::read_csv(paste0(
-                               path.package("dsvizprep"), "/inst/resources/stopwords.csv"
-                                    )
-                              )
+
+  if (stopwords) {
+  if (lang == "es")  stopwords <- stopwords_es$STOPWORD
+  if (lang == "en")  stopwords <- stopwords_en$STOPWORD
+  } else {
+    stopwords <- NULL
+  }
+
   if (any(class(data) %in% "character")) {
     if (is.null(ftype)) ftype <- "Cat"
     data <- gsub("[[:punct:]]", "", data)
     data <- gsub("[[:digit:]]", "", data)
     data <- gsub("\\s+", " ", data)
-    words <- data.frame(words = tokenizers::tokenize_words(data) %>% unlist())
-    data <- anti_join(words, stopwords, by = c("words" = "STOPWORD"))
+    data <- data.frame(words = tokenizers::tokenize_words(data, stopwords = stopwords) %>% unlist())
   }
   data
 }
